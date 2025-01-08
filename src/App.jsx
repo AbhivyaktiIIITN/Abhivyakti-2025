95a00c47f4a24fc0ef6dafca9115d61ed733b85f
Saksham Agrawal <agsaksham07@gmail.com>
2025-01-08 20:11:59 +0530

    hero and club selector layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import './css/app.css'
+import Event from './Pages/Event';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/event" element={<Event/>} />
             </Routes>
         </div>
     )
