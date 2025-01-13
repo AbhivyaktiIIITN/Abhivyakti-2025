39ad2d9022ce9dc86ff5f069681b41b30961ef08
Supratit <supratitdatta@gmail.com>
2025-01-14 00:31:11 +0530

    Corrected Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -13,29 +13,29 @@ function Footer() {
                 <div className="footer">
                     <div className="quick">
                         <h1>Quick Links</h1>
-                        <p><Link to="/" className="footer-link" target='blank'>Register</Link></p>
-                        <p><Link to="/event" className="footer-link" target='blank'>Explore</Link></p>
-                        <p><Link to="/" className="footer-link" target='blank'>Home</Link></p>
-                        <p><Link to="/terms" className="footer-link" target='blank'>Terms & Conditions</Link></p>
+                        <p><Link to="/event" className="footer-link" >Register</Link></p>
+                        <p><Link to="/event" className="footer-link" >Explore</Link></p>
+                        <p><Link to="/" className="footer-link" >Home</Link></p>
+                        <p><Link to="/terms" className="footer-link" >Terms & Conditions</Link></p>
                     </div>
                     <div className="contact">
                         <h1>Contact Us</h1>
-                        <p><Link to="/" className="footer-link" target='blank'>abhivyakti@iiitn.ac.in</Link></p>
-                        <p><Link to="/" className="footer-link" target='blank'>+91 6969696969</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>abhivyakti@iiitn.ac.in</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>+91 6969696969</Link></p>
                     </div>
                     <div className="follow">
                         <h1>Follow Us</h1>
-                        <p><Link to="/" className="footer-link" target='blank'>Instagram</Link></p>
-                        <p><Link to="/" className="footer-link" target='blank'>LinkedIn</Link></p>
-                        <p><Link to="/" className="footer-link" target='blank'>Twitter</Link></p>
-                        <p><Link to="/" className="footer-link" target='blank'>Facebook</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>Instagram</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>LinkedIn</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>Twitter</Link></p>
+                        <p><Link to="/" className="footer-link" target='_blank'>Facebook</Link></p>
                     </div>
                 </div>
             </div>
             <div className="copyright">
                 <div>Abhivyakti 2025 © All rights reserved.</div>
                 <div>
-                    Developed and Managed by <Link to="/developers" className="student" target='blank'>Students</Link>
+                    Developed and Managed by <Link to="/developers" className="student" target='_blank'>Students</Link>
                 </div>
             </div>
         </div>
