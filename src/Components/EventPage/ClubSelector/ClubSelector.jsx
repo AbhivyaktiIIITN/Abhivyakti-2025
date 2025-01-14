5fbe829bab5b4199d37a6a3b7a258494775f24b8
Saksham Agrawal <agsaksham07@gmail.com>
2025-01-14 23:30:05 +0530

    made club selector responsive

--- src/Components/EventPage/ClubSelector/ClubSelector.jsx
+++ src/Components/EventPage/ClubSelector/ClubSelector.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { FaIndianRupeeSign } from 'react-icons/fa6';
+import { clubs } from "./data"
 import "./clubSelector.css"
+import "../../../css/font.css"
 
 const ClubSelector = () => {
 
@@ -10,151 +12,101 @@ const ClubSelector = () => {
     const [hoverEvent, setHoverEvent] = useState(null)
     const [animate, setAnimate] = useState(false)
 
-    const clubs = [
-        {
-            color: "#FE5013",
-            exploreGradient: "",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        },
-        {
-            color: "#FF006E",
-            exploreGradient: "",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        },
-        {
-            name: "estoria",
-            artform: "drama",
-            color: "#9747FF",
-            exploreGradient: "#440977",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        },
-        {
-            color: "#2237C6",
-            exploreGradient: "",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                },
-                {
-                    title: "3"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        },
-        {
-            color: "#34FA46",
-            exploreGradient: "",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        },
-        {
-            color: "#FAF100",
-            exploreGradient: "",
-            events: [
-                {
-                    title: "1"
-                },
-                {
-                    title: "2"
-                },
-                {
-                    title: "3"
-                }
-            ]
-        }
-    ];
+
 
     const mouseEnter = (e, index) => {
-        e.target.style.width = '100%'
-        e.target.style.left = '0'
+
         e.target.style.zIndex = '50'
         e.target.style.transitionDelay = '0s'
         setHoverIndex(index)
         setHoverEvent(e)
         setAnimate(true)
 
-        const childs = e.target.querySelectorAll("p")
+        const artform = e.target.querySelector("#artform")
+        const clubName = e.target.querySelector("#club-name")
 
-        childs.forEach((child, index) => {
-            child.style.rotate = "90deg"
-            child.style.transitionDelay = '0s'
-        })
+        artform.style.transitionDelay = '0s'
+        clubName.style.transitionDelay = '0s'
+
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            e.target.style.width = '100%'
+            e.target.style.left = '0'
+
+            artform.style.bottom = '20rem'
+            artform.style.right = '46.5rem'
+            artform.style.rotate = '90deg'
+            artform.style.fontSize = '2rem'
+
+            clubName.style.bottom = '8rem'
+            clubName.style.left = '10.5rem'
+            clubName.style.right = '0rem'
+            clubName.style.rotate = '90deg'
+            clubName.style.fontSize = '12rem'
+        } else {
+            e.target.style.height = '100%'
+            e.target.style.top = '0'
+            
+            artform.style.bottom = '24rem'
+            artform.style.right = '9.5rem'
+            artform.style.fontSize = '2rem'
+
+            clubName.style.bottom = '17rem'
+            clubName.style.left = '5.5rem'
+            clubName.style.fontSize = '8rem'
+        }
     }
 
     const mouseOut = (e, index) => {
-        e.target.style.width = `calc((100% / ${clubs.length}) - 2px)`
-        e.target.style.left = `calc((100% / ${clubs.length}) * ${index} + ${index * 2}px)`
+
         e.target.style.zIndex = '0'
         e.target.style.transitionDelay = '0.5s'
-
         setAnimate(false)
 
-        const childs = e.target.querySelectorAll("p")
+        const artform = e.target.querySelector("#artform")
+        const clubName = e.target.querySelector("#club-name")
 
-        childs.forEach((child, index) => {
-            child.style.rotate = "0deg"
-            child.style.transitionDelay = '0.5s'
-        })
+        artform.style.transitionDelay = '0.5s'
+        clubName.style.transitionDelay = '0.5s'
+
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            e.target.style.width = `calc((100% / ${clubs.length}) - 2px)`
+            e.target.style.left = `calc((100% / ${clubs.length}) * ${index} + ${index * 2}px)`
+
+            artform.style.bottom = '27rem'
+            artform.style.right = '-0.75rem'
+            artform.style.rotate = '0deg'
+            artform.style.fontSize = '1rem'
+
+            clubName.style.bottom = '4rem'
+            clubName.style.left = '1.25rem'
+            clubName.style.right = '0rem'
+            clubName.style.rotate = '0deg'
+            clubName.style.fontSize = '8rem'
+        } else {
+            e.target.style.height = `calc((100% / ${clubs.length}) - 2px)`
+            e.target.style.top = `calc((100% / ${clubs.length}) * ${index})`
+            
+            artform.style.bottom = '5rem'
+            artform.style.right = '1.25rem'
+            artform.style.fontSize = '1rem'
+
+            clubName.style.bottom = '-0.5rem'
+            clubName.style.left = '1.5rem'
+            clubName.style.fontSize = '6rem'
+        }
     }
 
     return (
-        <div className="w-screen h-screen flex-col place-items-center">
+        <div className="w-screen h-screen flex-col place-items-center mt-52">
             <div
-                className='w-2/3 h-screen'
+                className='w-[95%] md:min-w-[768px] md:w-2/3 h-screen'
                 onMouseLeave={() => {
                     setClickedClub(null)
                     mouseOut(hoverEvent, hoverIndex)
                 }}
             >
                 <div
-                    className="w-full h-3/6 rounded-xl flex justify-between mb-2 items-center relative overflow-hidden"
+                    className="w-full h-full md:h-2/3 rounded-xl flex justify-between mb-2 items-center relative overflow-hidden"
                 // onMouseLeave={() => setClickedClub(null)}
                 >
                     {
@@ -162,27 +114,30 @@ const ClubSelector = () => {
                             <>
                                 <div
                                     key={index}
-                                    className="club-div h-full flex items-end justify-center rounded-xl absolute duration-500 cursor-pointer uppercase"
+                                    className="club-div md:h-full flex items-end justify-center rounded-xl absolute duration-500 cursor-pointer uppercase"
                                     style={{
+                                        '--index': index,
+                                        '--length': clubs.length,
                                         backgroundImage: `linear-gradient(140deg, white -10%, ${club.color})`,
                                         width: `calc((100% / ${clubs.length}) - 2px)`, // dividing by the number of clubs minus the gap
                                         left: `calc((100% / ${clubs.length}) * ${index} + ${index * 2}px)` // calculating position with gap
                                     }}
                                     onMouseEnter={(e) => mouseEnter(e, index)}
-                                    // onMouseLeave={(e) => mouseOut(e, index)}
                                     onClick={() => setClickedClub(club)}
                                 >
                                     {/* <img
-                                        className="h-full aspect-square"
+                                        className="w-[400px] -z-0 absolute right-0"
                                         src={`/assets/EventsPage/estoria/statue.png`}
                                     /> */}
                                     <p
-                                        className="-rotate-90 absolute top-8 -right-3 duration-500"
+                                        className="md:-rotate-90 absolute bottom-20 md:bottom-[27rem] right-5 md:-right-3 duration-500 urbanist-font text-white text-[1rem] drop-shadow-club-name"
+                                        id="artform"
                                     >
                                         {club.artform}
                                     </p>
                                     <p
-                                        className="font-semibold text-[50px] leading-tighter -rotate-90 absolute bottom-16 left-0 right-0 duration-500"
+                                        className="text-8xl md:text-9xl text-white drop-shadow-club-name md:-rotate-90 absolute -bottom-2 md:bottom-16 left-6 md:left-5 md:right-0 duration-500 humane-bold-font"
+                                        id="club-name"
                                     >
                                         {club.name}
                                     </p>
@@ -196,7 +151,7 @@ const ClubSelector = () => {
                         clickedClub && clickedClub.events.map((event, index) => (
                             <motion.div
                                 key={index}
-                                className="w-full h-48 flex rounded-xl mb-2 backdrop-blur-100"
+                                className="w-full h-32   xs:h-48 flex rounded-xl mb-2 backdrop-blur-100"
                                 // onMouseLeave={() => setClickedClub(null)}
                                 initial={{
                                     y: '-9rem',
@@ -229,27 +184,27 @@ const ClubSelector = () => {
                                     className="w-full h-full flex flex-col gap-0 rounded-e-xl p-5 uppercase text-[#FDFDFB]"
                                 >
                                     <div
-                                        className="w-full flex justify-between font-light text-[12px] text-[#C8C8C8]"
+                                        className="w-full flex justify-between font-light text-[10px] 2xs:text-[12px] text-[#C8C8C8]"
                                     >
                                         <p>offline</p>
                                         <p>prize pool</p>
                                     </div>
                                     <div
-                                        className="-mt-2 w-full flex justify-between font-bold text-[40px]"
+                                        className="-mt-1 xs:-mt-2 w-full flex justify-between font-bold text-sm 2xs:text-md xs:text-2xl sm:text-4xl"
                                     >
                                         <p>monoact</p>
-                                        <p className="flex items-baseline gap-1"><FaIndianRupeeSign size={20} /> 25,000</p>
+                                        <p className="flex items-baseline gap-1"><FaIndianRupeeSign className='text-xs sm:text-lg' /> 25,000</p>
                                     </div>
                                     <div
-                                        className="-mt-3 flex flex-col justify-between grow"
+                                        className="-mt-1 xs:-mt-2 flex flex-col justify-between grow"
                                     >
                                         <p
-                                            className="font-normal text-[20px]"
+                                            className="font-normal text-[0.6rem] 2xs:text-xs xs:text-md text-wrap sm:text-xl"
                                         >
                                             solo drama competition
                                         </p>
                                         <div
-                                            className="w-56 flex justify-between font-bold text-[14]"
+                                            className="w-full sm:w-56 flex justify-between font-bold text-[0.5rem] 2xs:text-xs sm:text-md"
                                         >
                                             <a
                                                 className="border-white border-2 rounded-md px-3 py-1"
---
+++ src/Components/EventPage/ClubSelector/data.js
@@ -0,0 +1,110 @@
+export const clubs = [
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#FE5013",
+        exploreGradient: "",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            }
+        ]
+    },
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#FF006E",
+        exploreGradient: "",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            }
+        ]
+    },
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#9747FF",
+        exploreGradient: "#440977",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            }
+        ]
+    },
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#2237C6",
+        exploreGradient: "",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            },
+            {
+                title: "3"
+            },
+            {
+                title: "3"
+            }
+        ]
+    },
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#34FA46",
+        exploreGradient: "",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            }
+        ]
+    },
+    {
+        name: "estoria",
+        artform: "drama",
+        color: "#FAF100",
+        exploreGradient: "",
+        events: [
+            {
+                title: "1"
+            },
+            {
+                title: "2"
+            },
+            {
+                title: "3"
+            }
+        ]
+    }
+];
