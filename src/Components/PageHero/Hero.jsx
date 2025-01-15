c7b6d0a2dc168022b22ab5f8b345dec42fe2ae04
Saksham Agrawal <agsaksham07@gmail.com>
2025-01-15 19:04:36 +0530

    made event page responsive

--- src/Components/PageHero/Hero.jsx
+++ src/Components/PageHero/Hero.jsx
@@ -2,36 +2,54 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence, useAnimate } from "framer-motion"
 import "../../css/font.css"
 
-const Hero = ({page}) => {
+const Hero = ({ page }) => {
 
-  // const [startAnimation, setStartAnimation] = useState(false)
   const [scope, animate] = useAnimate()
 
   const onMouseEnter = async (e) => {
 
-    // setStartAnimation(true)
+    if (window.matchMedia("(min-width: 768px)").matches) {
 
-    animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
-    await animate("#grafitti", { top: '-10rem', right: 0, left: '-8rem' }, { duration: 1 })
-    animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
-    await animate("#arrow", { top: '-10rem', right: 0, left: '-8rem', scaleX: -1, rotate: 360 }, { duration: 0.5 })
-    animate("#events", { bottom: '-5.5rem', left: '23rem', opacity: 1 }, { duration: 0.5 })
+      animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
+      await animate("#grafitti", { top: '-10rem', right: 0, left: '-8rem' }, { duration: 1 })
+      animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
+      await animate("#arrow", { top: '-10rem', right: 0, left: '-8rem', scaleX: -1, rotate: 360 }, { duration: 0.5 })
+      animate("#events", { bottom: '-5.5rem', left: '23rem', opacity: 1 }, { duration: 0.5 })
+
+    } else {
+
+      animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
+      await animate("#grafitti", { top: '-5rem', right: 0, left: '-5rem' }, { duration: 1 })
+      animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
+      await animate("#arrow", { top: '-5rem', right: 0, left: '-5rem', scaleX: -1, rotate: 360 }, { duration: 0.5 })
+      animate("#events", { bottom: '-2.5rem', left: '10rem', opacity: 1 }, { duration: 0.5 })
+    }
   }
 
   const onMouseLeave = async (e) => {
 
-    // setStartAnimation(false)
+    if (window.matchMedia("(min-width: 768px)").matches) {
 
-    await animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
-    animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
-    animate("#grafitti", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 1.5 })
-    // animate("#grafitti", { top: 500, left: "-50rem" }, { duration: 0.7 })
-    animate("#arrow", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 0.7 })
-    await animate("#events", { left: '-700%', top: '700%', opacity: 1 }, { duration: 0.5 })
-    // await animate("#events", { bottom: -500, left: '-50rem' }, { duration: 0.7 })
+      await animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
+      animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
+      animate("#grafitti", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 1.5 })
+      animate("#arrow", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 0.7 })
+      await animate("#events", { left: '-700%', top: '700%', opacity: 1 }, { duration: 0.5 })
 
-    animate("#arrow", { top: '-700%', right: 0, left: '-8rem', rotate: 180, scaleX: -1 }, { duration: 0 })
-    animate("#events", { left: '-5.5rem', bottom: '18rem', top: '14rem', opacity: 0 }, { duration: 0 })
+      animate("#arrow", { top: '-700%', right: 0, left: '-8rem', rotate: 180, scaleX: -1 }, { duration: 0 })
+      animate("#events", { left: '-5.5rem', bottom: '18rem', top: '14rem', opacity: 0 }, { duration: 0 })
+
+    } else {
+
+      await animate("#hand", { left: '-70%', top: '-70%' }, { duration: 1 })
+      animate("#hand", { left: '-700%', top: '700%' }, { duration: 1.5 })
+      animate("#grafitti", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 1.5 })
+      animate("#arrow", { left: '-700%', top: '700%', bottom: 0, right: 0 }, { duration: 0.7 })
+      await animate("#events", { left: '-700%', top: '700%', opacity: 1 }, { duration: 0.5 })
+
+      animate("#arrow", { top: '-700%', right: 0, left: '-8rem', rotate: 180, scaleX: -1 }, { duration: 0 })
+      animate("#events", { left: '5rem', bottom: '-2.5rem', top: '6.5rem', opacity: 0 }, { duration: 0 })
+    }
   }
 
   return (
@@ -39,7 +57,7 @@ const Hero = ({page}) => {
       className="w-screen h-screen flex justify-center items-center overflow-hidden"
     >
       <div
-        className="min-w-[300px] w-[50vw] sm:min-w-[600px] sm:w-[33vw] flex justify-center items-cente relative"
+        className="min-w-[300px] w-[50vw] sm:min-w-[600px] sm:w-[33vw] flex justify-center items-center relative"
         ref={scope}
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
@@ -64,14 +82,12 @@ const Hero = ({page}) => {
           id="grafitti"
         />
         <img
-          // className="w-[35rem] z-20 absolute rotate-180 scale-x-100 drop-shadow-arrow"
           className="w-[85%] z-20 absolute bottom-[700%] drop-shadow-arrow rotate-180 scale-x-100"
           src={`/assets/EventsPage/hero/arrow.png`}
           id="arrow"
         />
         <p
-          // className="text-8xl text-white absolute bottom-40 left-[33rem] rotate-[20deg] opacity-0"
-          className="text-8xl text-white absolute -bottom-[5.5rem] left-72 opacity-0 urbanist-font drop-shadow-title"
+          className="text-5xl sm:text-8xl text-white absolute -bottom-[2.5rem] left-20 sm:-bottom-[5.5rem] sm:left-72 opacity-0 urbanist-font drop-shadow-title"
           id="events"
         >
           {page}
