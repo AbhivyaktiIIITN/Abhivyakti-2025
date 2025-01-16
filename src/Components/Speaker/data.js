7f4c7eb9bdbe2800ee9493a426650ad47b8a36c4
Saksham <agsaksham07@gmail.com>
2025-01-16 08:40:58 +0530

    horizontal scroll element

---
+++ src/Components/Speaker/data.js
@@ -0,0 +1,18 @@
+export const speakers = [
+    {
+        name: "speaker xyz",
+        about: "about speaker/performance",
+        insta: "",
+        linkedin: "",
+        facebook: "",
+        image: "/assets/HomePage/temp2.png"
+    },
+    {
+        name: "speaker xyz",
+        about: "about speaker/performance",
+        insta: "",
+        linkedin: "",
+        facebook: "",
+        image: "/assets/HomePage/temp2.png"
+    },
+]
