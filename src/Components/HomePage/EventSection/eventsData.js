e73c4a38febcd5efac44c23dd513ab274f9bd1af
Supratit <supratitdatta@gmail.com>
2025-01-18 23:43:41 +0530

    Updated event card data

---
+++ src/Components/HomePage/EventSection/eventsData.js
@@ -0,0 +1,58 @@
+export const eventData = [
+    {
+        cardId: 1,
+        gradient: "linear-gradient(90deg, #fe6d3b, #FE5013)",
+        events: [
+            { name: "Innovation", desc: "Tech innovations and applications" },
+            { name: "AI", desc: "Transforming industries with AI" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+    {
+        cardId: 2,
+        gradient: "linear-gradient(90deg, #fc529c, #FF006E)",
+        events: [
+            { name: "Marketing", desc: "Effective strategies for startups" },
+            { name: "Branding", desc: "Building personal brands online" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+    {
+        cardId: 3,
+        gradient: "linear-gradient(90deg, #A658E0, #641AA5)",
+        events: [
+            { name: "Pitch", desc: "Compete and showcase startups" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+    {
+        cardId: 4,
+        gradient: "linear-gradient(90deg, #5d6cd3, #2237C6)",
+        events: [
+            { name: "Cybersecurity", desc: "Intro to protecting data" },
+            { name: "Privacy", desc: "Ethics of digital privacy" },
+            { name: "Protection", desc: "Securing data and systems" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+    {
+        cardId: 5,
+        gradient: "linear-gradient(90deg, #32CD32, #228B22)",
+        events: [
+            { name: "Sustainability", desc: "Eco-friendly innovations" },
+            { name: "Energy", desc: "Exploring renewable sources" },
+            { name: "Waste", desc: "Reducing waste in industries" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+    {
+        cardId: 6,
+        gradient: "linear-gradient(90deg, #f0ea40, #d0c903)",
+        events: [
+            { name: "Leadership", desc: "Master leadership skills" },
+            { name: "Negotiation", desc: "Enhance your negotiation tactics" },
+            { name: "Conflict", desc: "Resolving team conflicts effectively" },
+        ],
+        exploreLink: "https://unstop.com/"
+    },
+];
